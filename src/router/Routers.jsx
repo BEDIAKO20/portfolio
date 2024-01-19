e97d14c03e1fc8429ee6ef2bdccd27a5600b05da
Bediako-Mensah Kofi Boadi <boadikofi446@gmail.com>
2024-01-19 16:31:23 +0000

    first commit

---
+++ src/router/Routers.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home/Home'
+import About from '../pages/About/About'
+import Portfolio from "../pages/Portfolio/Portfolio"
+import Services from "../pages/services/Services"
+import Contact from "../pages/Contact us/Contact"
+
+
+function routers() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to="/home"/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default routers
