cb3b01e06b02ac74665f8a358c5b3cfef3db36e1
Bediako-Mensah Kofi Boadi <boadikofi446@gmail.com>
2024-01-22 17:39:49 +0000

    fff

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Typewriter from '../../components/Typewriter/Typewrite'
+import myImage  from "../../components/assets/kofiimage-removebg-preview.png"
 
 function Home() {
   return (
-    <div>
-      <h1>Hello it's working </h1>
+    <>
+      <div className='flex justify-center'>
+        <div className='ml-10 mt-11'>
+          <h3 className='text-3xl font-semibold text-gray-500 shadow-md'>
+       
+    <Typewriter text="I am Bediako-Mensah Kofi Boadi" delay={300} infinite/>
+          </h3>
+        </div>
+    <div className='' style={{marginLeft:"27%"}}>
+    <div className='bg-yellow-200 rounded-full w-96 h-96'>
+          <img src={myImage}/>
+        </div>
     </div>
-  )
+      </div>
+    </>
+  )             
 }
 
 export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    'node_modules/flowbite-react/lib/esm/**/*.js',
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js'
+   
   ],
   theme: {
     extend: {},
